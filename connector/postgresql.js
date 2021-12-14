cddb84020f25bca7a56d5032745e9a736cc28ae8
khanhsonb1c1 <khanhsonb1c1@gmail.com>
2021-12-14 11:13:38 +0700

    change conector info

--- connector/postgresql.js
+++ connector/postgresql.js
@@ -2,15 +2,23 @@ const { Pool } = require('pg')
 
 // POSTGRES_USER = 'postgres'
 // POSTGRES_HOST = 'localhost'
-// POSTGRES_DB = 'gears-shop'
+// POSTGRES_DB = 'gear-shop'
 // POSTGRES_PWD = '123456'
 // POSTGRES_PORT = '5432'
 
-POSTGRES_USER = 'zdxgdoyqugsxdh'
-POSTGRES_HOST = 'ec2-3-219-111-26.compute-1.amazonaws.com'
-POSTGRES_DB = 'dfnnq1mk0jdp6a'
-POSTGRES_PWD = 'af2744b3bd5d72a80b50ac27db50d8af3494449417522b563b602c1605324e68'
+POSTGRES_USER = 'yufvflhxcpvfgp'
+POSTGRES_HOST = 'ec2-44-196-146-152.compute-1.amazonaws.com'
+POSTGRES_DB = 'dsc1ubmbkprdf'
+POSTGRES_PWD = 'e3f6d097ceeec0a9630172a7c59156ffcb50c418dd9ab26dc4f1ce28103e4a0d'
 POSTGRES_PORT = '5432'
+
+//postgres://yufvflhxcpvfgp:e3f6d097ceeec0a9630172a7c59156ffcb50c418dd9ab26dc4f1ce28103e4a0d@ec2-44-196-146-152.compute-1.amazonaws.com:5432/dsc1ubmbkprdf
+
+// POSTGRES_USER = 'zdxgdoyqugsxdh'
+// POSTGRES_HOST = 'ec2-3-219-111-26.compute-1.amazonaws.com'
+// POSTGRES_DB = 'dfnnq1mk0jdp6a'
+// POSTGRES_PWD = 'af2744b3bd5d72a80b50ac27db50d8af3494449417522b563b602c1605324e68'
+// POSTGRES_PORT = '5432'
 
 //postgres://zdxgdoyqugsxdh:af2744b3bd5d72a80b50ac27db50d8af3494449417522b563b602c1605324e68@ec2-3-219-111-26.compute-1.amazonaws.com:5432/dfnnq1mk0jdp6a
 
@@ -23,6 +31,7 @@ const pgConfig = {
   port: POSTGRES_PORT,
   ssl: true
 }
+
 
 const pool = new Pool(pgConfig)
 /**
